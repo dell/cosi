887a813da0b2c4cfcc62abc58847c453a37d853c
Bartosz Ciesielczyk <bartosz.ciesielczyk@dell.com>
2023-02-06 14:41:17 +0100

    fix typos

--- tests/integration/bucket_access_revoke_suite_test.go
+++ tests/integration/bucket_access_revoke_suite_test.go
@@ -110,7 +110,7 @@ var _ = Describe("Bucket Access Revoke", Serial, Label("revoke"), func() {
 		steps.CreateNamespace(ctx, clientset, "namespace-1")
 
 		// STEP: COSI controller "objectstorage-controller" is installed in namespace "default"
-		By("Checking if COSI controller 'objectstorecontroller' is installed in namespace 'default'")
+		By("Checking if COSI controller 'objectstorage-controller' is installed in namespace 'default'")
 		steps.CheckCOSIControllerInstallation(ctx, clientset, "objectstorage-controller", "default")
 
 		// STEP: COSI driver "cosi-driver" is installed in namespace "driver-ns"
