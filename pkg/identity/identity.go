e68a538e87dcbb374af19a3f142544d6da6592b9
Mateusz Urbanek <110409467+shanduur-dell@users.noreply.github.com>
2023-02-16 10:03:50 +0100

    Include boilerplate code for COSI driver (#23)

---
+++ pkg/identity/identity.go
@@ -0,0 +1,50 @@
+//Copyright © 2023 Dell Inc. or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identity
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	cosi "sigs.k8s.io/container-object-storage-interface-spec"
+)
+
+type Server struct {
+	name string
+}
+
+var _ cosi.IdentityServer = (*Server)(nil)
+
+// FIXME: this is boilerplate, needs proper constructor
+func New(provisioner string) *Server {
+	return &Server{
+		name: provisioner,
+	}
+}
+
+func (srv *Server) DriverGetInfo(ctx context.Context,
+	req *cosi.DriverGetInfoRequest) (*cosi.DriverGetInfoResponse, error) {
+
+	if srv.name == "" {
+		log.Printf("Invalid argument: %v", errors.New("driver name cannot be empty"))
+		return nil, status.Error(codes.InvalidArgument, "DriverName is empty")
+	}
+
+	return &cosi.DriverGetInfoResponse{
+		Name: srv.name,
+	}, nil
+}
---
+++ pkg/identity/identity_test.go
@@ -0,0 +1,60 @@
+//Copyright © 2023 Dell Inc. or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package identity
+
+import (
+	"context"
+	"testing"
+
+	cosi "sigs.k8s.io/container-object-storage-interface-spec"
+)
+
+const (
+	provisioner = "test"
+)
+
+// FIXME: those are only smoke tests, no real testing is done here
+func TestDriverGetInfo(t *testing.T) {
+	for scenario, fn := range map[string]func(t *testing.T){
+		"smoke/testValidServer":            testValidServer,
+		"smoke/testMissingProvisionerName": testMissingProvisionerName,
+	} {
+		t.Run(scenario, func(t *testing.T) {
+			fn(t)
+		})
+	}
+}
+
+// FIXME: write valid test
+func testValidServer(t *testing.T) {
+	srv := New(provisioner)
+
+	res, err := srv.DriverGetInfo(context.TODO(), &cosi.DriverGetInfoRequest{})
+	if err != nil {
+		t.Errorf("got unexpected error: %s", err.Error())
+	}
+
+	if res.Name != provisioner {
+		t.Errorf("got: '%s', wanted '%s'", res.Name, provisioner)
+	}
+}
+
+// FIXME: write valid test
+func testMissingProvisionerName(t *testing.T) {
+	srv := &Server{}
+
+	_, err := srv.DriverGetInfo(context.TODO(), &cosi.DriverGetInfoRequest{})
+	if err == nil {
+		t.Errorf("got unexpected error: %s", err.Error())
+	}
+}
