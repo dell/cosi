ab925a0f21de0a3fb5fb2fa9e2490a5ff974ba82
Bartosz Ciesielczyk <bartosz.ciesielczyk@dell.com>
2023-03-13 11:56:34 +0100

    add more comments

--- core/core.go
+++ core/core.go
@@ -1,8 +1,6 @@
-//go:generate go run semver/semver.go -f semver.tpl -o core.gen.go
-
 package core
 
 var (
-	// SemVer is the semantic version.
+	// SemVer is the semantic version for use in `cosi-driver --version` command.
 	SemVer = "unknown"
 )
