6a31497b09f908dc754ea7b3073ef609db9a4049
Bartosz Ciesielczyk <bartosz.ciesielczyk@dell.com>
2023-03-30 12:22:15 +0200

    more test, and use channels for signaling server readiness

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -34,58 +34,102 @@ const (
 	COSISocket = "/var/lib/cosi/cosi.sock"
 )
 
-// Run starts the gRPC server for the identity and provisioner servers
-func Run(ctx context.Context, config *config.ConfigSchemaJson, socket, name string) error {
-	// Setup identity server and provisioner server
+// Representation of Driver for COSI API
+type Driver struct {
+	// gRPC Driver
+	server *grpc.Server
+	// socket listener
+	lis net.Listener
+}
+
+// NewDriver creates a new driver for COSI API with identity and provisioner servers
+func New(config *config.ConfigSchemaJson, socket, name string) (*Driver, error) {
+	// Setup identity Driver and provisioner Driver
 	identityServer := identity.New(name)
 
 	driverset := &provisioner.Driverset{}
 	for _, cfg := range config.Connections {
 		driver, err := provisioner.NewVirtualDriver(cfg)
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		err = driverset.Add(driver)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
 	provisionerServer := provisioner.New(driverset)
-	// Some options for gRPC server may be needed
+	// Some options for gRPC Driver may be needed
 	options := []grpc.ServerOption{}
-	// Crate new gRPC server
+	// Crate new gRPC Driver
 	server := grpc.NewServer(options...)
-	// Register identity and provisioner servers, so they will handle gRPC requests to the server
+	// Register identity and provisioner Drivers, so they will handle gRPC requests to the Driver
 	spec.RegisterIdentityServer(server, identityServer)
 	spec.RegisterProvisionerServer(server, provisionerServer)
 
 	// Remove socket file if it already exists
-	// so we can start a new server after crash or pod restart
+	// so we can start a new Driver after crash or pod restart
 	if _, err := os.Stat(socket); !errors.Is(err, fs.ErrNotExist) {
 		if err := os.RemoveAll(socket); err != nil {
 			log.Fatal(err)
 		}
 	}
 
-	// Create shared listener for gRPC server
-	lis, err := net.Listen("unix", socket)
+	// Create shared listener for gRPC Driver
+	listener, err := net.Listen("unix", socket)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Driver{server, listener}, nil
+}
+
+// Start starts the gRPC server and returns a channel that will be closed when it is ready
+func (s *Driver) Start(ctx context.Context) <-chan struct{} {
+	ready := make(chan struct{})
+	go func() {
+		close(ready)
+		if err := s.server.Serve(s.lis); err != nil {
+			log.Fatalf("Failed to serve gRPC server: %v", err)
+		}
+	}()
+	return ready
+}
+
+// Run starts the gRPC server for the identity and provisioner servers.
+// This function will not block and instead will provide channel for checking when the driver is ready.
+// Await for context if you want the thread you are running this in to block.
+func Run(ctx context.Context, config *config.ConfigSchemaJson, socket, name string) (<-chan struct{}, error) {
+	// Create new driver
+	driver, err := New(config, socket, name)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Infoln("gRPC server started")
+
+	return driver.Start(ctx), nil
+}
+
+// Blocking version of Run
+func RunBlocking(ctx context.Context, config *config.ConfigSchemaJson, socket, name string) error {
+	// Create new driver
+	driver, err := New(config, socket, name)
 	if err != nil {
 		return err
 	}
 
 	log.Infoln("gRPC server started")
-	// Run gRPC server in a separate goroutine
-	go func() {
-		if err := server.Serve(lis); err != nil {
-			log.Fatalf("Failed to serve gRPC server: %v", err)
-		}
-	}()
+	// Block until driver is ready
+	<-driver.Start(ctx)
 
-	// Wait for context cancellation or server error
+	// Block until context is done
 	<-ctx.Done()
-	server.GracefulStop()
+
+	// Gracefully stop the driver
+	driver.server.GracefulStop()
 
 	return nil
 }
--- pkg/driver/driver_test.go
+++ pkg/driver/driver_test.go
@@ -1,230 +1,207 @@
-// //Copyright © 2023 Dell Inc. or its subsidiaries. All Rights Reserved.
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //      http://www.apache.org/licenses/LICENSE-2.0
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+//Copyright © 2023 Dell Inc. or its subsidiaries. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//      http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 package driver
 
-// import (
-// 	"context"
-// 	"os"
-// 	"path"
-// 	"testing"
-// 	"time"
+import (
+	"context"
+	"os"
+	"path"
+	"testing"
+	"time"
 
-// 	"github.com/dell/cosi-driver/pkg/config"
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/dell/cosi-driver/pkg/config"
+	"github.com/stretchr/testify/assert"
+)
 
-// var (
-// 	testDir    = "test"
-// 	testRegion = "us-east-1"
+var (
+	testRegion = "us-east-1"
 
-// 	testConfig = &config.ConfigSchemaJson{}
+	testConfigEmpty = &config.ConfigSchemaJson{}
 
-// 	testConfigWithConnections = &config.ConfigSchemaJson{
-// 		Connections: []config.Configuration{
-// 			{
-// 				Objectscale: &config.Objectscale{
-// 					Credentials: config.Credentials{
-// 						Username: "testuser",
-// 						Password: "testpassword",
-// 					},
-// 					Id:                 "test.id",
-// 					ObjectscaleGateway: "gateway.objectscale.test",
-// 					ObjectstoreGateway: "gateway.objectstore.test",
-// 					Protocols: config.Protocols{
-// 						S3: &config.S3{
-// 							Endpoint: "s3.objectstore.test",
-// 						},
-// 					},
-// 					Region: &testRegion,
-// 					Tls: config.Tls{
-// 						Insecure: true,
-// 					},
-// 				},
-// 			},
-// 		},
-// 	}
+	testConfigWithConnections = &config.ConfigSchemaJson{
+		Connections: []config.Configuration{
+			{
+				Objectscale: &config.Objectscale{
+					Credentials: config.Credentials{
+						Username: "testuser",
+						Password: "testpassword",
+					},
+					Id:                 "test.id",
+					ObjectscaleGateway: "gateway.objectscale.test",
+					ObjectstoreGateway: "gateway.objectstore.test",
+					Protocols: config.Protocols{
+						S3: &config.S3{
+							Endpoint: "s3.objectstore.test",
+						},
+					},
+					Region: &testRegion,
+					Tls: config.Tls{
+						Insecure: true,
+					},
+				},
+			},
+		},
+	}
 
-// 	testConfigDuplicateID = &config.ConfigSchemaJson{
-// 		Connections: []config.Configuration{
-// 			{
-// 				Objectscale: &config.Objectscale{
-// 					Credentials: config.Credentials{
-// 						Username: "testuser",
-// 						Password: "testpassword",
-// 					},
-// 					Id:                 "test.id",
-// 					ObjectscaleGateway: "gateway.objectscale.test",
-// 					ObjectstoreGateway: "gateway.objectstore.test",
-// 					Protocols: config.Protocols{
-// 						S3: &config.S3{
-// 							Endpoint: "s3.objectstore.test",
-// 						},
-// 					},
-// 					Region: &testRegion,
-// 					Tls: config.Tls{
-// 						Insecure: true,
-// 					},
-// 				},
-// 			},
-// 			{
-// 				Objectscale: &config.Objectscale{
-// 					Credentials: config.Credentials{
-// 						Username: "testuser",
-// 						Password: "testpassword",
-// 					},
-// 					Id:                 "test.id",
-// 					ObjectscaleGateway: "gateway.objectscale.test",
-// 					ObjectstoreGateway: "gateway.objectstore.test",
-// 					Protocols: config.Protocols{
-// 						S3: &config.S3{
-// 							Endpoint: "s3.objectstore.test",
-// 						},
-// 					},
-// 					Region: &testRegion,
-// 					Tls: config.Tls{
-// 						Insecure: true,
-// 					},
-// 				},
-// 			},
-// 		},
-// 	}
+	testConfigDuplicateID = &config.ConfigSchemaJson{
+		Connections: []config.Configuration{
+			{
+				Objectscale: &config.Objectscale{
+					Credentials: config.Credentials{
+						Username: "testuser",
+						Password: "testpassword",
+					},
+					Id:                 "test.id",
+					ObjectscaleGateway: "gateway.objectscale.test",
+					ObjectstoreGateway: "gateway.objectstore.test",
+					Protocols: config.Protocols{
+						S3: &config.S3{
+							Endpoint: "s3.objectstore.test",
+						},
+					},
+					Region: &testRegion,
+					Tls: config.Tls{
+						Insecure: true,
+					},
+				},
+			},
+			{
+				Objectscale: &config.Objectscale{
+					Credentials: config.Credentials{
+						Username: "testuser",
+						Password: "testpassword",
+					},
+					Id:                 "test.id",
+					ObjectscaleGateway: "gateway.objectscale.test",
+					ObjectstoreGateway: "gateway.objectstore.test",
+					Protocols: config.Protocols{
+						S3: &config.S3{
+							Endpoint: "s3.objectstore.test",
+						},
+					},
+					Region: &testRegion,
+					Tls: config.Tls{
+						Insecure: true,
+					},
+				},
+			},
+		},
+	}
 
-// 	testConfigMissingObjectscale = &config.ConfigSchemaJson{
-// 		Connections: []config.Configuration{
-// 			{
-// 				Objectscale: nil,
-// 			},
-// 		},
-// 	}
-// )
+	testConfigMissingObjectscale = &config.ConfigSchemaJson{
+		Connections: []config.Configuration{
+			{
+				Objectscale: nil,
+			},
+		},
+	}
+)
 
-// func TestRun(t *testing.T) {
-// 	testCases := []struct {
-// 		name           string
-// 		config         *config.ConfigSchemaJson
-// 		auxiliaryFuncs []func(ctx context.Context) error
-// 		expectedError  bool
-// 	}{
-// 		{
-// 			name:          "success",
-// 			config:        testConfig,
-// 			expectedError: false,
-// 		},
-// 		{
-// 			name:          "success with connections",
-// 			config:        testConfigWithConnections,
-// 			expectedError: false,
-// 		},
-// 		{
-// 			name:          "failure duplicate ID",
-// 			config:        testConfigDuplicateID,
-// 			expectedError: true,
-// 		},
-// 		{
-// 			name:          "failure missing connection config",
-// 			config:        testConfigMissingObjectscale,
-// 			expectedError: true,
-// 		},
-// 	}
+func TestDriver(t *testing.T) {
+	t.Parallel()
+	for name, test := range map[string]func(t *testing.T){
+		"empty configuration":                             TestDriverEmptyConfiguration,
+		"configuration with connections":                  TestDriverConfigurationWithConnections,
+		"configuration with duplicate ID":                 TestDriverConfigurationWithDuplicateID,
+		"configuration with missing objectscale":          TestDriverConfigurationWithMissingObjectscale,
+		"with preexisting socket file":                    TestDriverWithPreexistingSocketFile,
+		"fail on non-existing socket directory":           TestDriverFailOnNonExistingDirectory,
+		"run blocking server":                             TestDriverRunBlockingServer,
+		"blocking server configuration with duplicate ID": TestDriverBlockingServerConfigurationWithDuplicateID,
+	} {
+		t.Run(name, test)
+	}
+}
 
-// 	for _, tc := range testCases {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			// Test server starts successfully and stops gracefully
-// 			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 			defer cancel()
+func TestDriverEmptyConfiguration(t *testing.T) {
+	t.Parallel()
+	// TODO: server should start successfully with an empty configuration?
+	err := runWithParameters(t, testConfigEmpty, t.TempDir())
+	assert.NoError(t, err)
+}
 
-// 			dir, err := os.MkdirTemp("", testDir)
-// 			if err != nil {
-// 				t.Fatal(err)
-// 			}
-// 			defer os.RemoveAll(dir)
+func TestDriverConfigurationWithConnections(t *testing.T) {
+	t.Parallel()
+	// server should start successfully with the provided configuration
+	err := runWithParameters(t, testConfigWithConnections, t.TempDir())
+	assert.NoError(t, err)
+}
 
-// 			errCh := make(chan error, 1)
-// 			go func() {
-// 				testSocketPath := path.Join(dir, "cosi.sock")
+func TestDriverConfigurationWithDuplicateID(t *testing.T) {
+	t.Parallel()
+	// server should fail if the configuration contains duplicate IDs
+	err := runWithParameters(t, testConfigDuplicateID, t.TempDir())
+	assert.Error(t, err)
+}
 
-// 				errCh <- Run(ctx, tc.config, testSocketPath, "test")
-// 			}()
+func TestDriverConfigurationWithMissingObjectscale(t *testing.T) {
+	t.Parallel()
+	// server should fail if the configuration is missing the objectscale connection
+	err := runWithParameters(t, testConfigMissingObjectscale, t.TempDir())
+	assert.Error(t, err)
+}
 
-// 			// Wait for server to start
-// 			time.Sleep(500 * time.Millisecond)
+func TestDriverWithPreexistingSocketFile(t *testing.T) {
+	t.Parallel()
+	dir := t.TempDir()
+	//create a socket file
+	socketPath := path.Join(dir, "cosi.sock")
+	_, err := os.Create(socketPath)
+	assert.NoError(t, err)
+	// server should delete the socket file and start with a new one
+	err = runWithParameters(t, testConfigWithConnections, dir)
+	assert.NoError(t, err)
+}
 
-// 			// Cancel context to stop server gracefully
-// 			cancel()
+func TestDriverFailOnNonExistingDirectory(t *testing.T) {
+	t.Parallel()
+	// server should fail if the directory does not exist
+	err := runWithParameters(t, testConfigWithConnections, "/nonexistent")
+	assert.Error(t, err)
+}
 
-// 			if tc.expectedError {
-// 				err := <-errCh
-// 				assert.Error(t, err)
-// 			} else {
-// 				err := <-errCh
-// 				assert.NoError(t, err)
-// 			}
-// 		})
-// 	}
-// }
+func TestDriverRunBlockingServer(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	err := RunBlocking(ctx, testConfigWithConnections, t.TempDir(), "test")
+	assert.NoError(t, err)
+}
 
-// func TestRunWithPreexistingSocketFile(t *testing.T) {
-// 	// Test server starts successfully and stops gracefully
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
+func TestDriverBlockingServerConfigurationWithDuplicateID(t *testing.T) {
+	t.Parallel()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	err := RunBlocking(ctx, testConfigDuplicateID, t.TempDir(), "test")
+	assert.Error(t, err)
+}
 
-// 	dir, err := os.MkdirTemp("", testDir)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	defer os.RemoveAll(dir)
+func runWithParameters(t *testing.T, configuration *config.ConfigSchemaJson, socketDirectoryPath string) error {
+	t.Helper()
 
-// 	errCh := make(chan error, 1)
-// 	go func() {
-// 		testSocketPath := path.Join(dir, "cosi.sock")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-// 		// Create preexisting socket file
-// 		_, err := os.Create(testSocketPath)
-// 		if err != nil {
-// 			errCh <- err
-// 		}
+	testSocketPath := path.Join(socketDirectoryPath, "cosi.sock")
+	ready, err := Run(ctx, configuration, testSocketPath, "test")
+	if err != nil {
+		return err
+	}
 
-// 		errCh <- Run(ctx, testConfig, testSocketPath, "test")
-// 	}()
+	// Block until server is ready
+	<-ready
+	// Cancel context to stop server gracefully
+	cancel()
 
-// 	// Wait for server to start
-// 	time.Sleep(500 * time.Millisecond)
-
-// 	// Cancel context to stop server gracefully
-// 	cancel()
-
-// 	err = <-errCh
-// 	assert.NoError(t, err)
-// }
-
-// func TestRunFailOnNonExistingDirectory(t *testing.T) {
-// 	// Test server starts successfully and stops gracefully
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
-
-// 	errCh := make(chan error, 1)
-// 	go func() {
-// 		testSocketPath := path.Join("/fake", "cosi.sock")
-
-// 		errCh <- Run(ctx, testConfig, testSocketPath, "test")
-// 	}()
-
-// 	// Wait for server to start
-// 	time.Sleep(500 * time.Millisecond)
-
-// 	// Cancel context to stop server gracefully
-// 	cancel()
-
-// 	err := <-errCh
-// 	assert.Error(t, err)
-// }
+	return nil
+}
