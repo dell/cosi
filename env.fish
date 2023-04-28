4264752d7e352f10a032b74ae9816aafbef9d370
Marcin Trajfacki <marcin.trajfacki@dell.com>
2023-04-28 08:34:06 -0400

    Cleanup

--- env.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/fish
-set -gx OBJECTSCALE_USER root
-set -gx OBJECTSCALE_PASSWORD ChangeMe
-set -gx OBJECTSCALE_GATEWAY https://10.227.149.255:31378
-set -gx OBJECTSCALE_OBJECTSTORE_GATEWAY https://10.227.149.255:32197
-set -gx KUBECONFIG /home/bartek/.kube/config_obs
