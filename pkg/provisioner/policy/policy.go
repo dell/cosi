5ab33b708055ec5f86cbdd0d91554a03b21d5d2e
Malgorzata Dutka <malgorzata.dutka@dell.com>
2023-08-11 06:11:32 -0400

    add annotations

--- pkg/provisioner/policy/policy.go
+++ pkg/provisioner/policy/policy.go
@@ -17,15 +17,15 @@ package policy
 import "encoding/json"
 
 type StatementEntry struct {
-	Effect    string
-	Action    []string
-	Resource  []string
-	Principal PrincipalEntry
-	Sid       string
+	Effect    string         `json:"Effect"`
+	Action    []string       `json:"Action"`
+	Resource  []string       `json:"Resource"`
+	Principal PrincipalEntry `json:"Principal"`
+	Sid       string         `json:"Sid"`
 }
 
 type PrincipalEntry struct {
-	AWS []string
+	AWS []string `json:"AWS"`
 }
 
 type Document struct {
