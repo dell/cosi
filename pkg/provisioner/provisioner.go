6ae17ae6816f51bfee21bf755fc912a7c23f2185
bogdanNovikovDell <119313007+bogdanNovikovDell@users.noreply.github.com>
2023-08-24 12:02:02 +0200

    Update pkg/provisioner/provisioner.go
    Co-authored-by: mdutka-dell <115641913+mdutka-dell@users.noreply.github.com>

--- pkg/provisioner/provisioner.go
+++ pkg/provisioner/provisioner.go
@@ -11,7 +11,7 @@
 // limitations under the License.
 
 // Package provisioner implements main COSI driver functionality
-// namely creating/deleing buckets and granting/revoking access to them.
+// namely creating/deleting buckets and granting/revoking access to them.
 package provisioner
 
 import (
