d28f27e317c14d6b85766a915ea6675316334c95
bogdanNovikovDell <119313007+bogdanNovikovDell@users.noreply.github.com>
2023-08-24 11:00:35 +0200

    Update pkg/provisioner/objectscale/objectscale.go
    Co-authored-by: Mateusz Urbanek <110409467+shanduur-dell@users.noreply.github.com>

--- pkg/provisioner/objectscale/objectscale.go
+++ pkg/provisioner/objectscale/objectscale.go
@@ -10,7 +10,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package objectscale implements custom server for communication with Objectscale.
+// Package objectscale implements custom server handlers for communication with Dell ObjectScale platform.
 package objectscale
 
 import (
