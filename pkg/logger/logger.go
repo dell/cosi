f8e72fdb2e9bd539b672982e1b93aff09c08abc1
Bartosz Ciesielczyk <bartosz.ciesielczyk@dell.com>
2023-08-29 14:14:42 +0200

    Add package doc for logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -10,8 +10,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package logger ...
-// TODO: write documentation comment for logger package
+// Package logger contains interface for logger
+// which allows easy switching between logger implementations.
 package logger
 
 import (
