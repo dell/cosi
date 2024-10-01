6f120e753f19f85241e65a5150abde22923fb29e
shaynafinocchiaro <shayna_finocchiaro@dell.com>
2024-10-01 11:40:00 -0400

    Update logger.go

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -58,7 +58,7 @@ func New(level int, formatter string) {
 	logrusInstance.SetLevel(logrus.Level(defaultLevel))
 
 	if level >= minLevel || level <= maxLevel {
-		logrusInstance.SetLevel(logrus.Level(uint(level)))
+		logrusInstance.SetLevel(logrus.Level(uint32(level)))
 	}
 
 	switch formatter {
